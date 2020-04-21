b7ee0e828a89a4aee4bc1d1948ca6a007caf926b
abhilash <abhilashgoyal4@gmail.com>
2020-04-21 11:47:27 +0530

    Question Link: https://leetcode.com/explore/other/card/30-day-leetcoding-challenge/528/week-1/3305/

--- src/main/java/ThirtyDayChallange/week3/Day6BSTFromPreOrder.java
+++ src/main/java/ThirtyDayChallange/week3/Day6BSTFromPreOrder.java
@@ -1,6 +1,8 @@
 package ThirtyDayChallange.week3;
 
 /**
+ * Question: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ * <p>
  * Return the root node of a binary search tree that matches the given preorder traversal.
  * <p>
  * (Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
